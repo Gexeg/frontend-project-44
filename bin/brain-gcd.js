eef7b653f1ba69ca87115e51c121271c1056ca34
Aleksandr <psiondnd@yandex.ru>
2024-09-23 17:01:43 +0500

    brain-gcd added

---
+++ bin/brain-gcd.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { launchGame, getUserName } from '../src/index.js';
+
+const getRandomNum = () => (Math.floor(Math.random() * 101) + 1);
+const getGcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+
+  return getGcd(b, a % b);
+};
+
+const askQuestionGcd = () => {
+  const a = getRandomNum();
+  const b = getRandomNum();
+  console.log(`Question: ${a} ${b}`);
+
+  const userAnswer = Number(readlineSync.question('Your answer: '));
+  const rightAnswer = getGcd(a, b);
+  return [userAnswer, rightAnswer];
+};
+
+const userName = getUserName();
+console.log('Find the greatest common divisor of given numbers.');
+launchGame(userName, askQuestionGcd);
